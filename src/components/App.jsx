9a9c9077657279d9b2bea425aeac27f269aa2f8b
YaroslavYu <yaroslavyu801@gmail.com>
2023-03-24 18:01:52 +0200

    finnaly

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,11 @@
+import { StyledAppContainer } from './App.styled';
+
+import { ImageGallery } from './ImageGallery/ImageGallery';
+
 export const App = () => {
   return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
+    <StyledAppContainer>
+      <ImageGallery />
+    </StyledAppContainer>
   );
 };
